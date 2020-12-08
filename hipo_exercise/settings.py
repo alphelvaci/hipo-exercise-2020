c0a692b15422b21525ce345ecf035810a2803271
Alp Helvaci <alphelvaci01@gmail.com>
2020-12-08 17:18:07 +0300

    Create the recipes app, and its models

--- hipo_exercise/settings.py
+++ hipo_exercise/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ recipes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.db.models import Count
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    def use_count(self):
+        return Ingredient.objects.filter(name=self.name).annotate(count=Count('recipe')).first().count
+
+class Recipe(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='static/images/%Y/%m/')
+    instructions = models.TextField()
+    difficulty_choices = [
+        ("easy", "Easy"),
+        ("medium", "Medium"),
+        ("hard", "Hard"),
+    ]
+    difficulty = models.CharField(max_length=10, choices=difficulty_choices, default="easy")
+    ingredients = models.ManyToManyField(Ingredient)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
